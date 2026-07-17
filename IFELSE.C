486990c81069bf9c745e58221b0eba2234ac32f6
Ishaan-Chauhan <ishaankchauhan@gmail.com>
2026-07-17 21:04:09 +0530

    Uploaded

---
+++ IFELSE.C
@@ -0,0 +1,53 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	   int n1,n2,n3;
+	   clrscr();
+	   printf("\nEnter the mark of student 1 -->");
+	   scanf("%d",&n1);
+	   printf("\nEnter the mark of student 2 -->");
+	   scanf("%d",&n2);
+	   printf("\nEnter the mark of student 3 -->");
+	   scanf("%d",&n3);
+
+	   if(n1>=80)
+	   {
+		printf("\nThe grade of student is A ");
+	   }//if1
+	   else if(n1>=60)
+		{
+		  printf("\nThe grade of student is B");
+		}
+		else
+		{
+			printf("\nThe stdent has failed");
+		}
+	    if(n1>=80)
+	   {
+		printf("\nThe grade of student is A ");
+	   }//if1
+	   else if(n1>=60)
+		{
+		  printf("\nThe grade of student is B");
+		}
+		else
+		{
+			printf("\nThe stdent has failed");
+		}
+
+	   if(n1>=80)
+	   {
+		printf("\nThe grade of student is A ");
+	   }//if1
+	   else if(n1>=60)
+		{
+		  printf("\nThe grade of student is B");
+		}
+		else
+		{
+			printf("\nThe stdent has failed");
+		}
+	   );
+
+}//main
