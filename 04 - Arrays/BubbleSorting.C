27f67f9165d05322640e736009906fc9176ff3bc
Ishaan-Chauhan <ishaankchauhan@gmail.com>
2026-09-17 19:57:29 +0530

    Sorting of an array using bubble sorting

---
+++ 04 - Arrays/BubbleSorting.C
@@ -0,0 +1,32 @@
+/*
+			Author - Ishaan
+			DOC - 20 Aug
+			Objective - Bubble Sorting
+
+*/
+#include<stdio.h>
+#include<conio.h>
+#define Size 10
+void main()
+{
+	int i,j,temp;
+	int a[Size]
+	clrscr();
+	printf("Enter the no. for sorting :\n");
+	scanf("%d",&a[i]);
+	for(i=0 ; i<Size ; i++)
+	{
+		for(j=0 ; j<Size - 1 ; j++)
+		{
+			if(a[j]<a[j+1])
+			{
+
+			}
+
+		}//for in
+
+	}//for out
+
+
+	getch();
+}//main
