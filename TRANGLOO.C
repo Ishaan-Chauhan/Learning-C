69e93f13950fcda2ba173eb5c750b9804166b799
Ishaan-Chauhan <ishaankchauhan@gmail.com>
2026-09-24 21:24:23 +0530

    Nested loop pattern program

--- TRANGLOO.C
+++ TRANGLOO.C
@@ -1,29 +1,32 @@
-/*
-			Author - Ishaan
-			DOC - 23 july
-			objective - nested loop (for)
+/* 
+ * Author - Ishaan 
+ * DOC - 23 July (Updated: 2026)
+ * Objective - nested loop (for) pattern generator
+ */ 
 
-*/
-void main()
-{
-	int i,j,n,t;
-	char ch;
-	clrscr();
-	printf("\nEnter the no.of rows : ");
-	scanf("%d",&n);
-	for(t=1;t<=4;t++)
-	{
-		printf("\nEnter the Character : ");
-		scanf(" %c",&ch);
-		for(i=1;i<=n;i++)
-		{
-			for(j=1;j<=i;j++)
-			{
-			printf("%c",ch);
-			}
-		printf("\n");
-		}
-	gotoxy(n+1,n+4);
-	}
-	getch();
-}//main
+#include <stdio.h>
+
+int main(void) {
+    int i, j, n, t;
+    char ch;
+
+    printf("Enter the no. of rows: ");
+    if (scanf("%d", &n) != 1) {
+        return 1; // Exit if input is invalid
+    }
+
+    for (t = 1; t <= 4; t++) {
+        printf("\nEnter the Character: ");
+        // The space before %c skips any leftover newline characters in the buffer
+        scanf(" %c", &ch); 
+
+        for (i = 1; i <= n; i++) {
+            for (j = 1; j <= i; j++) {
+                printf("%c", ch);
+            }
+            printf("\n");
+        }
+    }
+
+    return 0;
+}
