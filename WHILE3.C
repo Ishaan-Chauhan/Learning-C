d018c54b779dd9c994cb5f31fda8052878b86277
Ishaan-Chauhan <ishaankchauhan@gmail.com>
2026-09-24 21:17:44 +0530

    C program to print even numbers within a given range

--- WHILE3.C
+++ WHILE3.C
@@ -1,19 +1,21 @@
-void main()
-{
-	int no1,no2,i;
-	clrscr();
-	printf("\nEnter no1 : ");
-	scanf("%d",&no1);
-	printf("\nEnter no2 : ");
-	scanf("%d",&no2);
-	i=no1;
-	while(i<=no2)
-	{
-		if(i%2==0)
-		{
-			printf("\nI : %d",i);
-		}
-		i++;
-	}
-	getch();
+#include <stdio.h>
+
+int main() { 
+    int no1, no2, i; 
+    
+    printf("\nEnter no1 : "); 
+    scanf("%d", &no1); 
+    
+    printf("\nEnter no2 : "); 
+    scanf("%d", &no2); 
+    
+    i = no1; 
+    while(i <= no2) { 
+        if(i % 2 == 0) { 
+            printf("\nI : %d", i); 
+        } 
+        i++; 
+    } 
+    
+    return 0; 
 }
